908536de1730af1cb0cfa140b535316ffc2b25f2
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-06-22 19:17:45 -0700

    Create Categories.cs

---
+++ Categories.cs
@@ -0,0 +1,6 @@
+{
+    public enum Categories 
+    {
+        [Display(Name = "Clothing")] Clothing,
+        [Display(Name ="Food & Drink")] FoodAndDrink,
+        [Display(Name = "Health & Beauty")] HealthAndBeauty,
